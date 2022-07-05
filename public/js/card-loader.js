8d26ad9407e8a914eb9c98e6ea47870cd7015d1b
BRafters <brennanrafters@gmail.com>
2022-07-05 13:56:53 -0300

    Generated colors

--- public/js/card-loader.js
+++ public/js/card-loader.js
@@ -10,10 +10,8 @@ window.addEventListener("load", (ev) => {
   // Create the cards
   for (let i = 0; i < numOfCards; i++) {
     // Generate the color
-    color = generateColor();
-    console.log(color);
-    // Assign width
-    // Give hex values to other portion
+    color = "#" + generateColor();
+    addCard(color);
   }
 });
 
@@ -22,16 +20,45 @@ function generateWidthPercentage(num) {
   return (1 / num) * numOfCards;
 }
 
-function addCard() {
-  if (numOfCards == 8) {
-    window.alert("Maximum amount of color cards reached");
-    return;
-  }
-  
-  console.log(generateColor() + "\n" + numOfCards);
-  numOfCards++;
+/// Adds a card to view
+function addCard(color) {
+  let card = document.createElement("div");
+  card.classList.add(
+    "flex",
+    "justify-center",
+    "items-center",
+    "flex-col",
+    "rounded-md",
+    "h-full",
+    "m-2"
+  );
+  card.style.width = generateWidthPercentage(numOfCards) + "%";
+
+  let cardColor = document.createElement("div");
+  cardColor.classList.add("h-1/2", "w-full");
+  cardColor.style.backgroundColor = color;
+
+  let cardDetail = document.createElement("div");
+  cardDetail.classList.add(
+    "flex",
+    "justify-center",
+    "items-center",
+    "h-1/2",
+    "w-full",
+    "bg-off-white"
+  );
+
+  let h1 = document.createElement("h1");
+  h1.classList.add("font-display", "text-4xl");
+
+  card.appendChild(cardColor);
+  card.appendChild(cardDetail);
+  cardDetail.appendChild(h1);
+
+  CARD_PARENT.appendChild(card);
 }
 
+/// Removes a card from view
 function removeCard() {
   if (numOfCards == 3) {
     window.alert("Minimum amount of color cards reached");
@@ -42,4 +69,4 @@ function removeCard() {
   numOfCards--;
 }
 
-export {addCard, removeCard}
+export { addCard, removeCard };
